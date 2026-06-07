07fdbb4ed24e917638d0a2b9f3d713f96499e9d6
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-06-07 14:13:04 +0530

    app jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,12 @@
-import { useState } from "react";
+import Counter from "./Counter";
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
-    <div>
-      <h1>nasikki vidu sivakami</h1>
+    <>
+      <h1>React Learning</h1>
 
-      <h2>{count}</h2>
-      <h1>{setCount}</h1>
-
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        KOOTTAN ULLATH
-      </button>
-
-      <button
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        KURAKKAN ULLATH !
-      </button>
-      <button
-        onClick={() => {
-          setCount(0);
-        }}
-      >
-        VEEENDUM ZERO AAKKAN 
-      </button>
-    </div>
+      <Counter />
+    </>
   );
 }
 
