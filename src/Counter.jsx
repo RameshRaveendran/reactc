b7e9c40243364563cf995bd6306abf5d6d74478e
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-06-08 11:36:23 +0530

    counter update for props

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,34 +1,6 @@
-import { useState } from "react";
-
-function Counter() {
-  const [count, setCount] = useState(0);
-
+function Counter(props) {
   return (
-    <>
-      <h2>{count}</h2>
-
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        KOOTTAN
-      </button>
-      <button
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        KURAKKAN
-      </button>
-      <button
-        onClick={() => {
-          setCount(0);
-        }}
-      >
-        MUZHUVAN MAYKKAN
-      </button>
-    </>
+    <h1>{props.title}</h1>
   );
 }
 
