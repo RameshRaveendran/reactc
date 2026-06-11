eaa4256aea36f6669cb20edab4bf15f82fe4ec5d
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-06-11 17:21:36 +0530

    browser tittle update useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    console.log("Effect Ran");
-  });
+    document.title = `Count: ${count}`;
+  }, [count]);
 
   return (
     <>
       <h1>{count}</h1>
 
-      <button
-        onClick={() =>
-          setCount(count + 1)
-        }
-      >
-        Increment
-      </button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </>
   );
 }
