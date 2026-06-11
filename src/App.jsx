67d76f9cf704e38a2a9334eab7eb769b8b2f7568
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-06-11 17:15:56 +0530

    app updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,25 @@
 import { useState , useEffect } from "react";
 
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Effect Ran");
+  });
+
+  return (
+    <>
+      <h1>{count}</h1>
+
+      <button
+        onClick={() =>
+          setCount(count + 1)
+        }
+      >
+        Increment
+      </button>
+    </>
+  );
+}
+
+export default App;
