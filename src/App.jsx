0affb7d0ebe5ca66c22a2b43ac5b8474f7da7054
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-06-12 13:25:13 +0530

    next timer fuction setups

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(() => {
-    return Number(localStorage.getItem("count")) || 0;
-  });
+  const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    document.title = `Count: ${count}`;
-  });
-  useEffect(() => {
-    localStorage.setItem("count", count);
-  }, [count]);
-
-  useEffect(() => {
-    console.log("Count changed:", count);
-  }, [count]);
+  function startTimer() {
+    setInterval(() => {
+      setCount(prev => prev + 1);
+    }, 1000);
+  }
 
   return (
     <>
       <h1>{count}</h1>
 
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={startTimer}>
+        Start
+      </button>
     </>
   );
 }
