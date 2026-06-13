245ee0a886cff4464f252062a8d2f640456cb3d6
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-06-13 16:02:10 +0530

    app updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,51 @@
 import {
-  useRef,
-  useEffect
+  useState,
+  useRef
 } from "react";
 
 function App() {
 
+  const [notes, setNotes] =
+    useState([]);
+
   const inputRef =
     useRef(null);
 
-  useEffect(() => {
+  function addNote() {
+
+    const value =
+      inputRef.current.value;
+
+    if (!value) return;
+
+    setNotes([
+      ...notes,
+      value
+    ]);
+
+    inputRef.current.value = "";
 
     inputRef.current.focus();
-
-  }, []);
+  }
 
   return (
-    <input
-      ref={inputRef}
-      placeholder="Type..."
-    />
+    <>
+      <input
+        ref={inputRef}
+      />
+
+      <button
+        onClick={addNote}
+      >
+        Add Note
+      </button>
+
+      <ul>
+        {notes.map(note => (
+          <li>{note}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
