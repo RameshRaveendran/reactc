a8d69c8ebab66396965f6cc50727bf821342343f
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-06-18 22:10:20 +0530

    update done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,23 @@ function Home() {
   const { state } =
     useTheme();
 
+  const styles = {
+    background:
+      state.theme === "dark"
+        ? "#222"
+        : "#fff",
+
+    color:
+      state.theme === "dark"
+        ? "#fff"
+        : "#000",
+
+    padding: "30px",
+    minHeight: "100vh",
+  };
+
   return (
-    <div>
+    <div style={styles}>
       <h1>
         Home Page
       </h1>
