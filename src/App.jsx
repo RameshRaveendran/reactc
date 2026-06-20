f2a65934fd93fdf8e914c90a2718a3f267b06d83
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-06-20 16:48:09 +0530

    app udated

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,78 @@
-import Navbar
-from "./components/Navbar";
+import {
+  useState,
+  useEffect,
+} from "react";
 
-import Home
-from "./components/Home";
+import UserList
+from "./components/UserList";
 
 import {
-  ThemeProvider,
-} from "./context/ThemeContext";
+  getUsers,
+} from "./api/userApi";
 
 function App() {
 
+  const [users, setUsers] =
+    useState([]);
+
+  const [loading, setLoading] =
+    useState(true);
+
+  const [error, setError] =
+    useState("");
+
+  useEffect(() => {
+
+    async function fetchUsers() {
+
+      try {
+
+        const data =
+          await getUsers();
+
+        setUsers(data);
+
+      } catch (err) {
+
+        setError(
+          err.message
+        );
+
+      } finally {
+
+        setLoading(false);
+
+      }
+
+    }
+
+    fetchUsers();
+
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return (
+      <h1>
+        Error:
+        {error}
+      </h1>
+    );
+  }
+
   return (
-    <ThemeProvider>
+    <div>
+      <h1>
+        API Fetch Example
+      </h1>
 
-      <Navbar />
-
-      <Home />
-
-    </ThemeProvider>
+      <UserList
+        users={users}
+      />
+    </div>
   );
 }
 
