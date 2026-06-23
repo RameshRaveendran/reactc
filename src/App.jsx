8350fd207e3461435b5d4458cc81179b986584ac
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-06-23 19:54:30 +0530

    search added to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import UserList
 from "./components/UserList";
 
@@ -6,33 +8,56 @@ from "./hooks/useFetch";
 
 function App() {
 
+  const [search, setSearch] =
+    useState("");
+
   const {
     data: users,
     loading,
     error,
   } = useFetch(
- "https://jsonplaceholder.typicode.com/todos"
-);
+    "https://jsonplaceholder.typicode.com/users"
+  );
 
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
-
-  if (error) {
-    return (
-      <h1>Error: {error}</h1>
+  const filteredUsers =
+    users.filter(user =>
+      user.name
+        .toLowerCase()
+        .includes(
+          search.toLowerCase()
+        )
     );
-  }
+
+  if (loading)
+    return <h1>Loading...</h1>;
+
+  if (error)
+    return <h1>{error}</h1>;
 
   return (
     <div>
+
       <h1>
-        Custom Hook Example
+        User Search
       </h1>
 
-      <UserList
-        users={users}
+      <input
+        type="text"
+        placeholder="Search user..."
+        value={search}
+        onChange={(e) =>
+          setSearch(
+            e.target.value
+          )
+        }
       />
+
+      <UserList
+        users={
+          filteredUsers
+        }
+      />
+
     </div>
   );
 }
